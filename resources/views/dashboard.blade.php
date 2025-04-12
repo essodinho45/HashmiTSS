99141515c40111a376d8e9fc3d9f707d87acf90d
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2025-04-13 02:51:55 +0300

    fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,10 @@
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-2">
             <div
-                class="relative aspect-3/1 overflow-scroll rounded-xl border border-neutral-200 dark:border-neutral-700">
+                class="relative aspect-3/1 overflow-x-hidden rounded-xl border border-neutral-200 dark:border-neutral-700
+                [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100
+                [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300
+                dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
                 <h1 class="p-2">
                     {{ __('Open Tickets') }}
                 </h1>
