411c9b7cc075ab53d998af8e98419de6a333c550
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2025-04-14 09:23:39 +0300

    grids

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,24 +2,31 @@
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-2">
             <div
-                class="relative aspect-3/1 overflow-x-hidden rounded-xl border border-neutral-200 dark:border-neutral-700
-                [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100
+                class="relative aspect-3/1 overflow-x-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                {{-- [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100
                 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300
                 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
                 <h1 class="p-2">
                     {{ __('Open Tickets') }}
                 </h1>
-                <livewire:grids.open-tickets>
+                <livewire:grids.open-tickets-table> --}}
             </div>
             <div
-                class="relative aspect-3/1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+                class="relative aspect-3/1 overflow-x-hidden rounded-xl border border-neutral-200 dark:border-neutral-700
+                [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100
+                [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300
+                dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
                 <h1 class="p-2">
                     {{ __('Most Active Employees') }}
                 </h1>
+                <livewire:grids.active-employees-table>
             </div>
         </div>
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <h1 class="p-2">
+                {{ __('Latest Tickets') }}
+            </h1>
+            <livewire:grids.latest-tickets-table>
         </div>
     </div>
 </x-layouts.app>
